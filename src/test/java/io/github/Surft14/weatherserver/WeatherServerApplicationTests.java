c35c43cd4cc12ae1d46c1fda4d321fa4834aee02
SURFT!$ <mr.darck31@gmail.com>
2025-06-03 22:28:17 +0200

    Initial commit

---
+++ src/test/java/io/github/Surft14/weatherserver/WeatherServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Surft14.weatherserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
