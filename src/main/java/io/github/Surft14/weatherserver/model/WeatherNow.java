3b6e0148b0ee7f71e5d8441ec306e19e4d74b7e2
SURFT!$ <mr.darck31@gmail.com>
2025-06-04 09:42:20 +0200

    Added data class WeatherNow.java

---
+++ src/main/java/io/github/Surft14/weatherserver/model/WeatherNow.java
@@ -0,0 +1,24 @@
+package io.github.Surft14.weatherserver.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "weather_now")
+public class WeatherNow {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String city;
+    private LocalDateTime dateTime;
+
+    private Double temp;
+    private Double feelLike;
+    private String dir;
+    private Double speed;
+}
